c9ae70b5ed1d3c76a3b662ba7e8c5e9caae954ce
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-07-10 10:54:52 +0530

    Update C7 AA1 reference.py

--- C7 AA1 reference.py
+++ C7 AA1 reference.py
@@ -5,6 +5,11 @@ clock=pygame.time.Clock()
 
 screen = pygame.display.set_mode((450,300))
 
+img0 = pygame.image.load("objects/0.png")
+img1 = pygame.image.load("objects/1.png")
+img2 = pygame.image.load("objects/2.png")
+img3 = pygame.image.load("objects/3.png")
+
 b1 = pygame.Rect(50,100,50,50)
 b2 = pygame.Rect(150,100,50,50)
 b3 = pygame.Rect(250,100,50,50)
@@ -18,9 +23,9 @@ while True:
             pygame.quit()
             sys.exit()
     
-    pygame.draw.rect(screen,(0,0,0),b1)
-    pygame.draw.rect(screen,(0,0,0),b2)
-    pygame.draw.rect(screen,(0,0,0),b3)
-    pygame.draw.rect(screen,(0,0,0),b4)
+    screen.blit(img0, b1)
+    screen.blit(img1, b2)
+    screen.blit(img2, b3)
+    screen.blit(img3, b4)
     
     pygame.display.update()
